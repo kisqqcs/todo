bc143d0a3d6ff1905b09e0d9bba7442c78fc9571
kisqqcs <kisqqcs@yahoo.com>
2020-12-14 08:34:29 +0100

    LocalStorage working, pending items, buttons don't.

--- assets/script.js
+++ assets/script.js
@@ -1,137 +1,142 @@
-(function() {
-    // Mock data
-    let todos = [];
+'use strict'
+
     //Parts of date
     const bodyDay = document.querySelector('.body__day');
     const bodyDate = document.querySelector('.body__date');
-    
-    //Parts of todo input
-    const todoAddBtn = document.querySelector('.todo__btn');
-    const todoInput = document.querySelector(".todo__input");
-    const todoListPending = document.querySelector('.todo__list--pending');
-    
-    
-    //Parts of Pending Items
-    let TodoItemsNumber = document.querySelector(".todo__number")
-    
-    // Set Event Listeners
-    const setListeners = () => {
-        todoAddBtn.addEventListener('click', addNewTodo);
 
-    };
     //Names of the days
     const dayNames = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'];
-   
-        //Localstorage handler object.
-    const localDB = {
-        // localStorage.setItem('todos', todos);
-        setItem(key, value) {
-            value = JSON.stringify(value);
-            localStorage.setItem(key, value);
-        },
-        // localDB.getItem('todos');
-        getItem(key) {
-            const value = localStorage.getItem(key);
-            if(!value) {
-                return null;
-            }
-            return JSON.parse(value);
-        },
-        // localDB.removeItem('todos');
-        removeItem(key) {
-            localStorage.removeItem(key);
-        }
-    };
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'];
 
-    //Initialize application.
-    const init = () => {
-        showDate();
-        setListeners();
-        loadExistingTodos();
-        calculatePendingItemsNr();
-    };
-    
-    //Load existing todos.
-    const loadExistingTodos = () => {
-        const savedTodos = localDB.getItem('todos');
-        if (savedTodos) {
-            todos = savedTodos;
-        }
-        if (todos && Array.isArray(todos)) {
-            todos.forEach( todo => showTodo(todo));
-        }
-    };
-
-    //Show date
+       //Show date
     function showDate() {
-        const currentDate = new Date();
-        const day = [
-            currentDate.getMonth() + 1,
-            currentDate.getDate(),
-            currentDate.getFullYear(),
-        ].map(num => num < 10 ? `0${num}` : num);
+      const currentDate = new Date();
+      const day = [
+          currentDate.getMonth() + 1,
+          currentDate.getDate(),
+          currentDate.getFullYear(),
+      ].map(num => num < 10 ? `0${num}` : num);
 
-        bodyDay.textContent = dayNames[currentDate.getDay()];
-        bodyDate.textContent = day.join('-');
+      bodyDay.textContent = dayNames[currentDate.getDay()];
+      bodyDate.textContent = day.join('-');
 
+  };
+  showDate();
+
+    
+
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+let todos = [];
+let TodoItemsNumber = document.querySelector(".todo__number")
+
+//Event Listeners
+document.addEventListener('DOMContentLoaded', getToDos);
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions
+function addTodo(event){
+    event.preventDefault();
+    //ToDo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+    //Create Li
+    const newTodo = document.createElement('li');
+    // newTodo.innerText = todoInput.value;
+    newTodo.innerHTML = `<input type="checkbox"> ${todoInput.value}`;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Add ToDo to LocalStorage
+    saveLocalToDos(todoInput.value);
+    //Delete button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash" ></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    //Clear TodoInput value
+    todoInput.value = "";
+    };
+
+function deleteCheck(e) {
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add("fall");
+        removeLocalToDos(todo);
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
+    //Check mark
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    };
+}; 
 
-    //Save and add Todo in the Database
-    const addNewTodo =  () => {
-        const value = todoInput.value;
-        if (value === '') {
-            alert('Please type a todo');
-            return;
-        }
-        const todo = {
-            text: value,
-            done: false
-        };
-        todos.push(todo);
-        localDB.setItem('todos', todos);
-        showTodo(todo);
+  function saveLocalToDos(todo){
+      //Check Local Storage
+      let todos;
+      if(localStorage.getItem('todos') === null) {
+          todos = [];
+      }else{
+          todos = JSON.parse(localStorage.getItem('todos'));
+      }
+      todos.push(todo);
+      localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  function getToDos () {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo) {
+    //ToDo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerHTML = `<input type="checkbox"> ${todo}`;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash" ></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+    });
+};
+
+function removeLocalToDos(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+      }else{
         calculatePendingItemsNr();
-        todoInput.value = '';
-    };
-
-    // Show todo in the list
-    const showTodo = todo => {
-        const todoItem = document.createElement('div');
-        todoListPending.appendChild(todoItem);
-        
-        todoItem.innerHTML = `
-            <input type="checkbox">
-            <span>${todo.text}</span>   
-            <button class="trash--btn">
-                <i class="fa fa-trash"></i>
-            </button>
-        `;
-        const deleteTodoBtn = document.querySelector(".trash--btn");
-        deleteTodoBtn.addEventListener('click', deleteTodo);
-    };
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
     //Pending items number (még nincsen benne a done!!!)
     const calculatePendingItemsNr = () => {
-        TodoItemsNumber.textContent= Object.keys(todos).length;
-    };
-
-    //Delete todos (1. törli a div-et 1.törli az objektumból az elemet)
-        const deleteTodo = (e) => {
-            const item = e.target;
-            if(item.classList[0] === 'trash--btn') {
-                const todo = item.parentElement;
-                todo.remove();
-            };
-            
-        };
-
-    
-    init();
-})();
-
+     TodoItemsNumber.textContent= Object.keys(todos).length;
+  };
